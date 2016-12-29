69a766d64cb4e5c509624e09f1c9a55c22f13266
zallek <nicolas.fortin37@gmail.com>
2016-12-29 14:40:43 +0100

    Introduce getStats

--- src/groupPerfs.js
+++ src/groupPerfs.js
@@ -20,8 +20,41 @@ module.exports.get = function(event, content, cb) {
   });
 };
 
+module.exports.stats = function(event, content, cb) {
+  var params = {
+    TableName: 'galaxy-monitoring-groupPerfs',
+  };
 
-module.exports.post = function(event, content, cb) {
+  dynamoDb.scan(params, function(err, data) {
+    if (err) {
+      return cb(new Error('[500] Error on dynamoDb read'));
+    } else {
+      var stats = {
+        count: data.Count,
+        urls: { avg: 0, min: 0, max: 0 },
+        links: { avg: 0, min: 0, max: 0 },
+      };
+
+      var urlsTotal = 0, linksTotal = 0;
+      data.Items.forEach(function(item, i) {
+        urlsTotal += item.urls;
+        stats.urls.max = i ? Math.max(stats.urls.max, item.urls) : item.urls;
+        stats.urls.min = i ? Math.min(stats.urls.min, item.urls) : item.urls;
+
+        linksTotal += item.links;
+        stats.links.max = i ? Math.max(stats.links.max, item.links) : item.links;
+        stats.links.min = i ? Math.min(stats.links.min, item.links) : item.links;
+      })
+      stats.urls.avg = urlsTotal / stats.count;
+      stats.links.avg = linksTotal / stats.count;
+
+      return cb(null, stats);
+    }
+  });
+};
+
+
+module.exports.create = function(event, content, cb) {
   var data = event.body;
   if (!data || !data.urls || !data.links) {
     return cb(new Error('[400] Missing or invalid urls/links properties in body'));
