449d073a5bd0fe0ac20b99c7ccfffaa4976bdd29
Nicolas Fortin <nicolas.fortin37@gmail.com>
2017-01-17 20:33:07 +0100

    Group stats by analysis

--- src/groupPerfs.js
+++ src/groupPerfs.js
@@ -20,6 +20,33 @@ module.exports.get = function(event, content, cb) {
   });
 };
 
+function computeStats(items) {
+  var nbRecords = items.length;
+  var urls = { avg: 0, min: 0, max: 0 };
+  var links = { avg: 0, min: 0, max: 0 };
+
+  var urlsTotal = 0, linksTotal = 0;
+  items.forEach(function(item, i) {
+    urlsTotal += item.urls;
+    urls.max = i ? Math.max(urls.max, item.urls) : item.urls;
+    urls.min = i ? Math.min(urls.min, item.urls) : item.urls;
+
+    linksTotal += item.links;
+    links.max = i ? Math.max(links.max, item.links) : item.links;
+    links.min = i ? Math.min(links.min, item.links) : item.links;
+  })
+  urls.avg = urlsTotal / nbRecords;
+  links.avg = linksTotal / nbRecords;
+
+  return {
+    nbRecords: nbRecords,
+    stats: {
+      urls: urls,
+      links: links,
+    },
+  };
+}
+
 module.exports.stats = function(event, content, cb) {
   var params = {
     TableName: 'galaxy-monitoring-groupPerfs',
@@ -29,24 +56,18 @@ module.exports.stats = function(event, content, cb) {
     if (err) {
       return cb(new Error('[500] Error on dynamoDb read'));
     } else {
-      var stats = {
-        count: data.Count,
-        urls: { avg: 0, min: 0, max: 0 },
-        links: { avg: 0, min: 0, max: 0 },
-      };
+      var itemsByAnalysis = {};
+      data.Items.forEach(function(item) {
+        if (!itemsByAnalysis[item.analysisId]) {
+          itemsByAnalysis[item.analysisId] = [];
+        }
+        itemsByAnalysis[item.analysisId].push(item);
+      });
 
-      var urlsTotal = 0, linksTotal = 0;
-      data.Items.forEach(function(item, i) {
-        urlsTotal += item.urls;
-        stats.urls.max = i ? Math.max(stats.urls.max, item.urls) : item.urls;
-        stats.urls.min = i ? Math.min(stats.urls.min, item.urls) : item.urls;
-
-        linksTotal += item.links;
-        stats.links.max = i ? Math.max(stats.links.max, item.links) : item.links;
-        stats.links.min = i ? Math.min(stats.links.min, item.links) : item.links;
-      })
-      stats.urls.avg = urlsTotal / stats.count;
-      stats.links.avg = linksTotal / stats.count;
+      var stats = {};
+      Object.keys(itemsByAnalysis).forEach(function(key) {
+        stats[key] = computeStats(itemsByAnalysis[key]);
+      });
 
       return cb(null, stats);
     }
